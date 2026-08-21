fae8d19f70cc4321b1a8a4af4c071ccbd41ba19d
RLT145 <angeltoo2016@gmail.com>
2026-08-20 22:04:08 -0600

    Actualizando configuracion de Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // Esto le dice a Vercel que ignore los warnings de código
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    // Esto le dice a Vercel que ignore los errores de tipos de TypeScript
     ignoreBuildErrors: true,
   },
 };
